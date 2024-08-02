9fd7f2f0564540964ed4de3f3b450ad43a2bfd73
Grant Smith <hello@grvnt.dev>
2024-08-02 10:08:43 +0200

    Quartz sync: Aug 2, 2024, 10:08 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 A Wiki for Leo",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
